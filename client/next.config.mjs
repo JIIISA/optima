7e1148fe31bce1057182b25ce4a31b62b1cfccb8
JIIISA <138257942+JIIISA@users.noreply.github.com>
2024-10-13 00:21:12 +0200

    init

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "optimas3-images.s3.eu-north-1.amazonaws.com/",
+        hostname: "optimas3-images.s3.eu-north-1.amazonaws.com",
         port: "",
-        pathname: "/**",
-      }
-    ]
-  }
+        pathname: "/**", // Dette matcher alle filer i rodmappen
+      },
+    ],
+  },
 };
 
 export default nextConfig;
